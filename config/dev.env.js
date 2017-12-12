8a1c9102b53315776002ad2350ab9aa46ef11239
xueshi.zhang <598846720@qq.com>
2017-12-12 20:39:50 +0800

    增加dist

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"//192.168.13.13:8080/ssm/model"',
+  // BASE_API: '"//192.168.13.39:8010/ssm/model"',
+  BASE_API: '"//192.168.13.186:8080/ssm/model"',// 线上186
   API_HOST: '"/api/"'
 })
--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://192.168.13.13:8058/',
+        target: 'http://192.168.13.186:8080/ssm/model',
         changeOrigin: true,
         pathRewrite: {
           '^/api': '/'
@@ -21,7 +21,7 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
+    host: '0.0.0.0', // can be overwritten by process.env.HOST  // 修改本地可以用ip打开
     port: 8881, // can be overwritten by process.env.HOST, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
